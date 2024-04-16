99501c7750d93eddd4c52388d522932865f9c903
DIOGO-CORREIA\diogo.correia <diogo.correia@inm.pt>
2024-04-16 15:37:36 +0100

    Revert "Forcing invalidate to false"
    This reverts commit aa9ae93396243381dba76b9279294085a4e22477.

--- src/android/BiometricActivity.java
+++ src/android/BiometricActivity.java
@@ -70,7 +70,7 @@ public class BiometricActivity extends AppCompatActivity {
             throw new CryptoException(PluginError.BIOMETRIC_ARGS_PARSING_FAILED);
         }
         Cipher cipher = mCryptographyManager
-                .getInitializedCipherForEncryption(mPromptInfo.getSecretKey(), false, this);
+                .getInitializedCipherForEncryption(mPromptInfo.getSecretKey(), invalidateOnEnrollment, this);
         mBiometricPrompt.authenticate(createPromptInfo(), new BiometricPrompt.CryptoObject(cipher));
     }
 
