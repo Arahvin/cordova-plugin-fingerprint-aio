c1dc9f82b0abf3e1e636227b220d659a6cbc43ad
DIOGO-CORREIA\diogo.correia <diogo.correia@inm.pt>
2024-04-17 10:38:23 +0100

    Adding logs

--- src/android/CryptographyManagerImpl.java
+++ src/android/CryptographyManagerImpl.java
@@ -167,6 +167,7 @@ class CryptographyManagerImpl implements CryptographyManager {
             byte[] plaintext = cipher.doFinal(ciphertext);
             return new String(plaintext, StandardCharsets.UTF_8);
         } catch (Exception e) {
+            Log.d("CORDOVA_BIOMETRIC decryptData", e.getMessage());
             throw new CryptoException(e.getMessage(), e);
         }
     }
