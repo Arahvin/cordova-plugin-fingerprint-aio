38083ebccea5a9648ffcb307233071c35976f939
DIOGO-CORREIA\diogo.correia <diogo.correia@inm.pt>
2024-04-17 16:10:59 +0100

    Adding default value

--- src/ios/Fingerprint.swift
+++ src/ios/Fingerprint.swift
@@ -143,7 +143,7 @@ enum PluginError:Int {
         let data  = command.arguments[0] as AnyObject?;
         var pluginResult: CDVPluginResult
         do {
-            let secret = Secret(keyName: data?.object(forKey: "secretKey") as? String)
+            let secret = Secret(keyName: (data?.object(forKey: "secretKey") as? String) ?? "__aio_key")
             try? secret.delete()
             let invalidateOnEnrollment = (data?.object(forKey: "invalidateOnEnrollment") as? Bool) ?? false
             try secret.save(secretStr, invalidateOnEnrollment: invalidateOnEnrollment)
@@ -165,7 +165,7 @@ enum PluginError:Int {
         }
         var pluginResult: CDVPluginResult
         do {
-            let secret = Secret(keyName: data?.object(forKey: "secretKey") as? String)
+            let secret = Secret(keyName: (data?.object(forKey: "secretKey") as? String) ?? "__aio_key")
             let result = try secret.load(prompt)
             pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result);
         } catch {
@@ -247,9 +247,7 @@ class Secret {
     private var keyName: String = "__aio_key"
 
     init(keyName: String) {
-        if(keyName) {
-            self.keyName = keyName
-        }
+        self.keyName = keyName
     }
 
     private func getBioSecAccessControl(invalidateOnEnrollment: Bool) -> SecAccessControl {
